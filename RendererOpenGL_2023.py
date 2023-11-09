83f69ebafe8c857064d57c9bcab913f7953268e3
jsantis <san21153@uvg.edu.gt>
2023-11-09 00:57:15 -0600

    implementacion de shaders completa

--- RendererOpenGL_2023.py
+++ RendererOpenGL_2023.py
@@ -1,81 +1,83 @@
+
 import pygame
+import glm
 from pygame.locals import *
 from gl import Renderer
 from model import Model
 from shaders import *
-import glm
+from obj import Obj
 
 
-width = 960 
-height = 540 
+width = 960
+height = 540
 
 pygame.init()
-
-
 screen = pygame.display.set_mode((width, height), pygame.OPENGL | pygame.DOUBLEBUF)
-
 clock = pygame.time.Clock()
 
-rend = Renderer(screen)
+gl = Renderer(screen)
+gl.setShader(vertex_shader, fragment_shader)
 
-rend.setShaders(vertex_shader, fragment_shader)
-
-
-#                  positions            UVs             NORMALS 
-triangleData = [-0.5, -0.5, 0.0,     0.0, 0.0,      0.0, 0.0, 1.0,
-                 -0.5, 0.5, 0.0,     0.0, 1.0,      0.0, 0.0, 1.0,
-                 0.5, -0.5, 0.0,     1.0, 0.0,      0.0, 0.0, 1.0,
-                 
-                 
-                 -0.5, 0.5, 0.0,     0.0, 1.0,      0.0, 0.0, 1.0,
-                  0.5, 0.5, 0.0,     1.0, 0.0,      0.0, 0.0, 1.0,
-                 0.5, -0.5, 0.0,     1.0, 0.0,      0.0, 0.0, 1.0 ]
-
-            
-
-triangleModel = Model(triangleData)
-triangleModel.loadTexture('textures/caja.jpg')
-triangleModel.position.z = -2
-triangleModel.scale = glm.vec3(2,2,2)
+obj = Obj("objs/golem.obj")
+objData = obj.objData
 
 
 
-rend.scene.append(triangleModel)
-
+model = Model(objData)
+model.loadTexture("textures/golem.jpeg")
+model.position.z = -6
+model.position.y = -2
+model.scale = glm.vec3(0.7, 0.7, 0.7)
+gl.scene.append(model)
+gl.lightIntensity = 5.5
+gl.dirLight = glm.vec3(0.0, -1.0, -1.0)
 
 isRunning = True
-
 while isRunning:
+    deltaTime = clock.tick(60) / 1000.0
+    gl.elapsedTime += deltaTime
     keys = pygame.key.get_pressed()
-    deltaTime = clock.tick(60) / 1000
+
+    if keys[K_RIGHT]:
+        gl.clearColor[0] += deltaTime
+    if keys[K_LEFT]:
+        gl.clearColor[0] -= deltaTime
+    if keys[K_UP]:
+        gl.clearColor[1] += deltaTime
+    if keys[K_DOWN]:
+        gl.clearColor[1] -= deltaTime
+    if keys[K_SPACE]:
+        gl.clearColor[2] += deltaTime
+    if keys[K_LSHIFT]:
+        gl.clearColor[2] -= deltaTime
+
+    if keys[K_d]:
+        model.rotation.y += deltaTime * 50
+    if keys[K_a]:
+        model.rotation.y -= deltaTime * 50
+    if keys[K_w]:
+        model.rotation.x += deltaTime * 50
+    if keys[K_s]:
+        model.rotation.x -= deltaTime * 50
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             isRunning = False
-
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 isRunning = False
-
-    if keys[K_RIGHT]:
-        rend
-        rend.camPosition.x += 10 * deltaTime
-    elif keys[K_LEFT]:
-        rend.camPosition.x -= 10 * deltaTime
-
-    if keys[K_UP]:
-        rend.camPosition.y += 10 * deltaTime
-    elif keys[K_DOWN]:
-        rend.camPosition.y -= 10 * deltaTime
-
-    if keys[K_a]:
-        rend.camPosition.z += 10 * deltaTime
-    elif keys[K_d]:
-        rend.camPosition.z -= 10 * deltaTime
-
-    rend.elapsedTime += deltaTime
-
-    rend.render()
+            if event.key == pygame.K_1:
+                gl.setShader(vertex_shader, fragment_shader)
+            if event.key == pygame.K_2:
+                gl.setShader(vertex_shader, greyscale_fragment_shader)
+            if event.key == pygame.K_3:
+                gl.setShader(vertex_shader, Fresnel_shader)
+            if event.key == pygame.K_4:
+                gl.setShader(vertex_shader, noise_shader)
+            if event.key == pygame.K_5:
+                gl.setShader(vertex_shader, stripes_shader)
+    gl.render()
     pygame.display.flip()
 
 pygame.quit()
--- shaders.py
+++ shaders.py
@@ -1,42 +1,107 @@
-# En OpenGl los shaders se escriben en un nuevo lenguaje de programacion llamado GLSL
-# (OpenGL Shading Language). Los shaders son programas que corren en la GPU y son
-# los encargados de procesar los vertices y los px de la escena. Los shaders
-
-vertex_shader = '''
-#version 450 core 
-layout (location = 0) in vec3 position;
-layout (location = 1) in vec2 textCoords;
-layout (location = 2) in vec3 normals;
-
-uniform mat4 modelMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 projectionMatrix;
 
 
-out vec2 Uvs;
-
-void main(){
-
-
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+vertex_shader = """
+    #version 450 core
+    layout (location = 0) in vec3 position;
+    layout (location = 1) in vec2 texCoords;
+    layout (location = 2) in vec3 normals;
     
-    Uvs = textCoords;
-}
+    uniform mat4 modelMatrix;
+    uniform mat4 viewMatrix;
+    uniform mat4 projectionMatrix;
+    
+    out vec2 UVs;
+    out vec3 normal;
+    
+    void main() {
+        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+        UVs = texCoords;
+        normal = (modelMatrix * vec4(normals, 0.0)).xyz;
+    }
+"""
 
-'''
+fragment_shader = """
+    #version 450 core
+    
+    layout (binding = 0) uniform sampler2D tex;
+    
+    in vec2 UVs;
+    in vec3 normal;
+    out vec4 fragColor;
+    
+    void main() {
+        fragColor = texture(tex, UVs);
+    }
+"""
 
-fragment_shader = '''
-#version 450 core
-in vec2 Uvs;
+greyscale_fragment_shader = """
 
-uniform sampler2D basicTexture;
+    #version 450 core
 
-out vec4 FragColor;
+    layout (binding = 0) uniform sampler2D tex;
 
-void main(){
-    FragColor = texture(basicTexture, Uvs);
-}
+    in vec2 UVs;
+    in vec3 normal;
+    out vec4 fragColor;
 
+    void main() {
 
+        vec4 color = texture(tex, UVs);
+        float grey = (color.r + color.g + color.b) / 3.0;
+        fragColor = vec4(grey, grey, grey, 1.0);
+    }
+"""
 
-'''
+Fresnel_shader = """
+
+    #version 450 core
+
+    layout (binding = 0) uniform sampler2D tex;
+
+    in vec2 UVs;
+    in vec3 normal;
+    out vec4 fragColor;
+
+    void main() {
+
+        vec4 color = texture(tex, UVs);
+        float fresnel = pow(1.0 - dot(normal, vec3(0.0, 0.0, -1.0)), 2.0);
+        fragColor = vec4(color.rgb * fresnel, 1.0);
+    }
+"""
+
+noise_shader = """  
+    #version 450 core
+
+    layout (binding = 0) uniform sampler2D tex;
+
+    in vec2 UVs;
+    in vec3 normal;
+    out vec4 fragColor;
+
+    void main() {
+            
+            vec4 color = texture(tex, UVs);
+            float noise = fract(sin(dot(UVs, vec2(12.9898,78.233))) * 43758.5453);
+            fragColor = vec4(color.rgb * noise, 1.0);
+        }
+"""
+
+stripes_shader = """
+    #version 450 core
+
+    layout (binding = 0) uniform sampler2D tex;
+
+    in vec2 UVs;
+    in vec3 normal;
+    out vec4 fragColor;
+
+    void main() {
+                
+                vec4 color = texture(tex, UVs);
+                float stripes = step(0.2, fract(UVs.x * 5.0));
+                fragColor = vec4(color.rgb * stripes, 1.0);
+            }
+
+"""   
+
